f837e479a8e44ad54a613bf8e4463efe03e72957
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-24 08:46:18 -0700

    initial commit

---
+++ EmpWageArrayInheritance/EmpWageArrayInheritance/EmpWage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageArrayInheritance
+{
+    class EmpWage
+    {
+    }
+}
---
+++ EmpWageArrayInheritance/EmpWageArrayInheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//****Calculate the different company monthly hrs daily wage is 50 part time 4hrs and full time 8hrs use array using inheritance****
+
+namespace EmpWageArrayInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
