b43aafe1717f149862913989a1a4f646e8ef26ee
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-24 17:09:52 -0700

    Added EmpWage

--- EmpWageArrayInheritance/EmpWageArrayInheritance/EmpWage.cs
+++ EmpWageArrayInheritance/EmpWageArrayInheritance/EmpWage.cs
@@ -10,99 +10,89 @@ namespace EmpWageArrayInheritance
     {
         public const int Full_Time = 1;
         public const int Part_Time = 2;
-        public string Company_name;
+        public string Company_Name;
         public int Wage_Per_Hr;
         public int Full_Hr_Per_Day = 8;
         public int Part_Hr_Per_Day = 4;
         public int Max_Working_Days;
         public int Max_Working_Hrs;
         public int wagesPerMonth = 0;
-        public EmpWage[] companies;
-       public int compCount;
+        public int[] companies;
+        public int compCount;
 
-        public EmpWage(string Company_Name, int Wage_Per_Hr, int Full_Hr_Per_Day, int Part_Hr_Per_Day, int Max_Working_Days, int Max_Working_Hrs)
+        public EmpWage( int Wage_Per_Hr, int Full_Hr_Per_Day, int Part_Hr_Per_Day, int Max_Working_Days, int Max_Working_Hrs)
         {
-            this.Company_name = Company_Name;
+            
             this.Wage_Per_Hr = Wage_Per_Hr;
             this.Full_Hr_Per_Day = Full_Hr_Per_Day;
             this.Part_Hr_Per_Day = Part_Hr_Per_Day;
             this.Max_Working_Hrs = Max_Working_Hrs;
             this.Max_Working_Days = Max_Working_Days;
-            companies = new EmpWage[2];
+            companies = new int[2];
             compCount = 0;
         }
-     
-
 
     }
 
     public class EmpWageArray : EmpWage
-        {
-
-        string Company_Name;
-        public EmpWageArray() : base("Company_Name",50, 8, 4, 20, 160)
-        {
-
-        }
-          public int Calculations()
     {
-            EmpWage company = new EmpWage(Company_Name,Wage_Per_Hr, Full_Hr_Per_Day, Part_Hr_Per_Day, Max_Working_Days, Max_Working_Hrs);
-        int Hr_Per_Day = 0;
-        int Wage_Per_Day = 0;
-        int Total_Working_Hrs = 0;
-        int Present_Days = 0;
-        int Monthly_Wage = 0;
-
-
-
-        while (Total_Working_Hrs < company.Max_Working_Hrs && Present_Days < company.Max_Working_Days)
+        public EmpWageArray() : base( 50, 8, 4, 20, 160)
         {
-               
-            switch (new Random().Next(0, 3))
+
+        }
+        public int Calculations()
+        {
+            EmpWage company = new EmpWage( Wage_Per_Hr, Full_Hr_Per_Day, Part_Hr_Per_Day, Max_Working_Days, Max_Working_Hrs);
+            int Hr_Per_Day = 0;
+            int Wage_Per_Day = 0;
+            int Total_Working_Hrs = 0;
+            int Present_Days = 0;
+            int Monthly_Wage = 0;
+
+
+
+            while (Total_Working_Hrs < company.Max_Working_Hrs && Present_Days < company.Max_Working_Days)
             {
-                case Full_Time:
-                    Hr_Per_Day = company.Full_Hr_Per_Day;
-                    Present_Days++;
-                    break;
-                case Part_Time:
-                    Hr_Per_Day = company.Part_Hr_Per_Day;
-                    Present_Days++;
-                    break;
-                default:
-                    Hr_Per_Day = 0;
-                    break;
 
+                switch (new Random().Next(0, 3))
+                {
+                    case Full_Time:
+                        Hr_Per_Day = company.Full_Hr_Per_Day;
+                        Present_Days++;
+                        break;
+                    case Part_Time:
+                        Hr_Per_Day = company.Part_Hr_Per_Day;
+                        Present_Days++;
+                        break;
+                    default:
+                        Hr_Per_Day = 0;
+                        break;
+
+                }
+                Total_Working_Hrs += Hr_Per_Day;
+                Wage_Per_Day = (company.Wage_Per_Hr * Hr_Per_Day);
+                Monthly_Wage += Wage_Per_Day;
             }
-            Total_Working_Hrs += Hr_Per_Day;
-            Wage_Per_Day = (company.Wage_Per_Hr * Hr_Per_Day);
-            Monthly_Wage += Wage_Per_Day;
+            return Monthly_Wage;
         }
-        return Monthly_Wage;
 
-    }
-
-        public void MonthlyWages(int Monthly_Wage)
-        {
-            this.wagesPerMonth = Monthly_Wage;
-            Console.WriteLine($"Wages for the company {Company_name} for the month : {wagesPerMonth}");
-        }
-        public void AddCompany(string Company_Name, int Wage_Per_Hr, int Full_Hr_Per_Day, int Part_Hr_Per_Day, int Max_Working_Days, int Max_Working_Hrs)
+        
+        public  void AddCompany(string Company_Name)
 
         {
-            EmpWageArray company = new EmpWageArray();
-            company.MonthlyWages(this.Calculations());
-            companies[compCount] = company;
+            this.Company_Name = Company_Name;
+            
+            EmpWageArray company = new EmpWageArray();            
+            companies[compCount] = this.Calculations();
+            Console.WriteLine($"Monthly Employee Wage in {Company_Name} is { companies[compCount]}");
             compCount++;
 
-
-
         }
+        
+
 
     }
 
 
 
 }
-
-
-
